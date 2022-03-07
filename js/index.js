056c12e68d1de15e122bbb8dee1ea2a741c981c4
Garciarline <88305122+Garciarline@users.noreply.github.com>
2022-03-07 14:31:07 -0600

    I was up until 5am last night and got nowhere
    tried jquery stuff with my tutor

--- js/index.js
+++ js/index.js
@@ -1,6 +1,14 @@
 console.log("your page is loading")
 
-$(button). on ('click' function (){
+$(button).on('click',function (){
     $('#light')
     console.log("hey look a click occured")
-}
+});
+$(document).ready(function(){
+    $("button").click(function(){
+      $("div").animate({left: '250px'});
+    });
+  });
+
+
+<button>Start Animation</button>
